851666dbe5e6744949282c4f847d34f3da062d78
madmalio <madmalio82@gmail.com>
2026-08-10 17:07:50 -0600

    feat: automatically start and stop mediamtx on boot

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -11,6 +12,25 @@ import (
 	"tvapp/internal/api"
 	"tvapp/internal/db"
 )
+
+func startMediaMTX() *exec.Cmd {
+	bin := "mediamtx"
+	if _, err := os.Stat("./mediamtx"); err == nil {
+		bin = "./mediamtx"
+	} else if _, err := os.Stat("./mediamtx.exe"); err == nil {
+		bin = "./mediamtx.exe"
+	}
+
+	cmd := exec.Command(bin)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		log.Printf("Warning: failed to start mediamtx (%s): %v", bin, err)
+		return nil
+	}
+	log.Printf("Started mediamtx with PID %d", cmd.Process.Pid)
+	return cmd
+}
 
 func main() {
 	if err := db.Init("tvapp.db"); err != nil {
@@ -27,11 +47,17 @@ func main() {
 
 	srv := &http.Server{Addr: ":" + port, Handler: router}
 
+	mtxCmd := startMediaMTX()
+
 	go func() {
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		<-quit
 		log.Println("shutting down...")
+		if mtxCmd != nil && mtxCmd.Process != nil {
+			log.Println("stopping mediamtx...")
+			mtxCmd.Process.Signal(syscall.SIGTERM)
+		}
 		srv.Close()
 	}()
 
